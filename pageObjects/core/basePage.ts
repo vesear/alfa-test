91ba2c42ea3cd43d34d4b3eda2541d2d8a3310d5
vesear <veselkoartyom@gmail.com>
2023-12-04 19:44:18 +0100

    TC-5: Refactor empty cart test

--- pageObjects/core/basePage.ts
+++ pageObjects/core/basePage.ts
@@ -2,8 +2,8 @@ import { NavBar } from './components/navBar';
 import { Page } from '@playwright/test';
 
 export abstract class BasePage {
+    public readonly NavBar;
     protected readonly page: Page;
-    protected readonly NavBar;
 
     protected constructor(readonly _page: Page) {
         this.page = _page;
