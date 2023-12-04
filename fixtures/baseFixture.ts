8f62d1ae92888a6e2f2c0b666776a1921cb997da
vesear <veselkoartyom@gmail.com>
2023-12-04 19:43:30 +0100

    TC-5: Added CartPage to baseFixture.ts

--- fixtures/baseFixture.ts
+++ fixtures/baseFixture.ts
@@ -1,11 +1,13 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pageObjects/loginPage';
 import { MainPage } from '../pageObjects/mainPage/mainPage';
+import { CartPage } from '../pageObjects/cartPage';
 import { user } from '../config';
 
-export type PageObjects = {
+type PageObjects = {
     loginPage: LoginPage;
     mainPage: MainPage;
+    cartPage: CartPage;
 };
 
 export const test = base.extend<PageObjects>({
@@ -20,4 +22,11 @@ export const test = base.extend<PageObjects>({
         const mainPage = new MainPage(page);
         await use(mainPage);
     },
+
+    cartPage: async ({ page }, use) => {
+        const cartPage = new CartPage(page);
+        await use(cartPage);
+    },
 });
+
+export { expect } from '@playwright/test';
